fc1e1bf2e1475d115e34271ca7a3d34ad5aa4bd6
aspatel262 <110962081+aspatel262@users.noreply.github.com>
2022-08-11 18:26:04 -0400

    Finished basic structure and functionality of game

--- hangman.py
+++ hangman.py
@@ -1,4 +1,11 @@
+from dataclasses import replace
 import random
+from os import system, name
+from time import sleep
+
+def clear():
+    _=system('clear')
+
 
 random.seed(a=None, version=2)
 
@@ -65,6 +72,48 @@ def getWord(wordList):
 
 def printHangman(wrongGuesses):
     print(hangmanStages[wrongGuesses])
+    
+def guessLetter(word, wrongGuesses, currGuess):
+    guess = input("Guess a Letter: ")
+    if guess in word:
+        i = 0
+        for l in word:
+            if guess == l:
+                currGuess = setCurrGuess(currGuess, i, guess)
+            i+=1
+    else:
+        print("WRONG!!!!")
+        wrongGuesses+=1
+    return(currGuess, wrongGuesses)
+    
+def setCurrGuess(currGuess, place, guess):
+    currGuess=currGuess.replace(" ", "")
+    currGuessList = list(currGuess)
+    currGuessList[place] = guess
+    currGuess = " ".join(currGuessList)
+    return currGuess
+
+def printCurrGuess(currGuess):
+    print()
+    print()
+    print(currGuess)
+    print()
+    print()
+    
+def guessWord(word):
+    return
+
+def winnner():
+    return
+
+def loser():
+    return
+
+def displayLetters():
+    return
+
+def repeatGuess():
+    return
 
 play = True
 win = False
@@ -75,15 +124,42 @@ while play:
     
     wrongGuesses = 0
     
+    currGuess = ""
+    for letter in gameWord:
+        currGuess = currGuess + '_ '
+    
     while wrongGuesses < 6 and not win:
         print()
         printHangman(wrongGuesses)
+        printCurrGuess(currGuess)
+        currGuessTest = currGuess.replace(" ","")
+        if currGuessTest==gameWord:
+            #winscreenfunction
+            print("win")
+            break
+        else:
+            pass
+        retTup = guessLetter(gameWord, wrongGuesses, currGuess)
+        currGuess = retTup[0]
+        wrongGuesses = retTup[1]
+        sleep(1)
+        clear()
         
+    if wrongGuesses == 6:
+        #displayloserfunction
+        printHangman(6)
+        print()
+        print("LOSER")
+        print()
+        print("the word was: " + gameWord)
         
-        wrongGuesses+=1
+    #if guessed already guessed char throw message, if win throw win screen, if lose trhow lose screen
+    #if guess wrong letter throw wrong screen
+    #display guessed letters
         
-    print("Enter yes to play again.")
-    ans = input()
+    
+        
+    ans = input("Enter 'yes' to play again, leave blank to quit:    ")
     if ans == "yes":
         wrongGuesses=0
     else:
